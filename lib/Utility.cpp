thierryseegers/LeapAsl

commit 5d19a9929e28586b3af4a268f8ee9a90dd3bf5c1

Extract translation to origin from normalized_hand_transform.

lib/Utility.cpp
@@ -74,9 +74,6 @@ Leap::Matrix normalized_hand_transform(Leap::Hand const& hand)
     float const scale_factor = 100. / hand.palmWidth();
     normalized *= {{scale_factor, 0, 0}, {0, scale_factor, 0}, {0, 0, scale_factor}};   // Normalize scale to 10 cm. palm width.
     
-    
-    normalized *= {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, -hand.palmPosition()}; // Translate to origin.
-    
     return normalized;
 }
     

recorder/main.cpp
@@ -255,23 +255,25 @@ int main()
         auto const& hand = controller.frame().hands()[0];
         if(hand.isValid())
         {
-            // Draw the hand with its original rotation and scale but offset to the left a tad.
+            // Draw the hand with its original rotation and scale but offset to the left of the origin.
             auto const offset = Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, -hand.palmPosition() + Leap::Vector{-200, 0, 0}};
             drawTransformedSkeletonHand(hand, offset, LeapUtilGL::GLVector4fv{1, 0, 0, 1});
 
-            // Draw the hand normalized.
+            // Draw the hand normalized and at the origin.
             auto const normalized = LearnedGestures::normalized_hand_transform(hand);
-            drawTransformedSkeletonHand(hand, normalized, LeapUtilGL::GLVector4fv{0, 1, 0, 1});
+            auto const centered = Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, -hand.palmPosition()};
+            drawTransformedSkeletonHand(hand, normalized * centered, LeapUtilGL::GLVector4fv{0, 1, 0, 1});
         }
         
         // Draw the replay hand.
         if(replay_hand.isValid())
         {
             // Draw the replay hand normalized and offset to the right a tad.
-            auto const offset = Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, Leap::Vector{+200, 0, 0}};
+            auto const centered = Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, -replay_hand.palmPosition()};
             auto const normalized = LearnedGestures::normalized_hand_transform(replay_hand);
+            auto const offset = Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, Leap::Vector{+200, 0, 0}};
             
-            drawTransformedSkeletonHand(replay_hand, offset * normalized, LeapUtilGL::GLVector4fv{0, 0, 1, 1});
+            drawTransformedSkeletonHand(replay_hand, offset * normalized * centered, LeapUtilGL::GLVector4fv{0, 0, 1, 1});
         }
 
         // Finally, display the rendered frame on screen.

transcriber/main.cpp
@@ -264,26 +264,27 @@ int main()
         window.popGLStates();
         
         auto const& hand = controller.frame().hands()[0];
-        
         if(hand.isValid())
         {
-            // Draw the hand with its original rotation and scale but offset to the left a tad.
+            // Draw the hand with its original rotation and scale but offset to the left of the origin.
             auto const offset = Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, -hand.palmPosition() + Leap::Vector{-200, 0, 0}};
             drawTransformedSkeletonHand(hand, offset, LeapUtilGL::GLVector4fv{1, 0, 0, 1});
-
-            // Draw the hand normalized.
+            
+            // Draw the hand normalized and at the origin.
             auto const normalized = LearnedGestures::normalized_hand_transform(hand);
-            drawTransformedSkeletonHand(hand, normalized, LeapUtilGL::GLVector4fv{0, 1, 0, 1});
+            auto const centered = Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, -hand.palmPosition()};
+            drawTransformedSkeletonHand(hand, normalized * centered, LeapUtilGL::GLVector4fv{0, 1, 0, 1});
         }
         
         // Draw the replay hand.
-        if(replay_character.getString() != "" && replay_hand.isValid())
+        if(replay_hand.isValid())
         {
             // Draw the replay hand normalized and offset to the right a tad.
-            auto const offset = Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, Leap::Vector{+200, 0, 0}};
+            auto const centered = Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, -replay_hand.palmPosition()};
             auto const normalized = LearnedGestures::normalized_hand_transform(replay_hand);
+            auto const offset = Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, Leap::Vector{+200, 0, 0}};
             
-            drawTransformedSkeletonHand(replay_hand, offset * normalized, LeapUtilGL::GLVector4fv{0, 0, 1, 1});
+            drawTransformedSkeletonHand(replay_hand, offset * normalized * centered, LeapUtilGL::GLVector4fv{0, 0, 1, 1});
         }
 
         // Finally, display the rendered frame on screen.

commit 2399c3fd1f0c9c678987c19f7c234b7f069cc767

Fix bug introduced by 5d19a99.
LeapAsl::to_position needed hand to be centered as well.

lib/Utility.cpp
@@ -33,7 +33,7 @@ fingers_position to_position(Leap::Hand const& hand)
 {
     fingers_position fp;
     
-    auto const normalized = normalized_hand_transform(hand);
+    auto const normalized_centered = normalized_hand_transform(hand) * Leap::Matrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, -hand.palmPosition()};
     auto const& fingers = hand.fingers();
     
     for(int f = 0; f != 5; ++f)
@@ -46,12 +46,12 @@ fingers_position to_position(Leap::Hand const& hand)
             
             if(b == Leap::Bone::TYPE_METACARPAL)
             {
-                fp[f][b] += normalized.transformPoint(bone.prevJoint());
-                fp[f][b + 1] = normalized.transformPoint(bone.nextJoint());
+                fp[f][b] += normalized_centered.transformPoint(bone.prevJoint());
+                fp[f][b + 1] = normalized_centered.transformPoint(bone.nextJoint());
             }
             else
             {
-                fp[f][b + 1] = normalized.transformPoint(bone.nextJoint());
+                fp[f][b + 1] = normalized_centered.transformPoint(bone.nextJoint());
             }
         }
     }