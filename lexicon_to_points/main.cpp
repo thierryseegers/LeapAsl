384d2f6ee3545278abe8523da82fd831bcdcdfcb
Thierry Seegers <surfintheusa@yahoo.com>
2016-07-27 10:52:36 -0700

    Add lexicon_to_points binary project.

---
+++ lexicon_to_points/main.cpp
@@ -0,0 +1,33 @@
+#include "LeapAsl/Lexicon.h"
+
+#include <LeapSDK/Leap.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    LeapAsl::Lexicon lexicon;
+    cin >> lexicon;
+
+    for(auto const& name : lexicon.names())
+    {
+        for(auto const& hand : lexicon.hands(name))
+        {
+            cout << name;
+            
+            for(auto const& finger : LeapAsl::to_position(hand))
+            {
+                for(auto const& joint : finger)
+                {
+                    cout << ',' << joint.x << ',' << joint.y << ',' << joint.z;
+                }
+            }
+            
+            cout << '\n';
+        }
+    }
+
+    return 0;
+}
